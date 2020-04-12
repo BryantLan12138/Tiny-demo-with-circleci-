d119c72823a59b1a93fdd8ca5c47c15823281d9a
BryantLan12138 <lubi980835699@gmail.com>
2020-04-12 15:16:21 +1000

    added pack

--- test/unit/task.test.js
+++ test/unit/task.test.js
@@ -16,7 +16,7 @@ describe('models/task', function () {
     it('creates a task', function () {
       return this.Users.create({ username: 'johndoe' }).bind(this).then(function (user) {
         return this.Tasks.create({ title: 'a title', UserId: user.id }).then(function (task) {
-          expect(task.title).to.equal('an title');
+          expect(task.title).to.equal('a title');
         });
       });
     });

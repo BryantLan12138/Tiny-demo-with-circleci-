4eff2f2ba62749888eec6bf8a47af117bb2d8038
BryantLan12138 <lubi980835699@gmail.com>
2020-04-13 10:48:16 +1000

    added test result after unit test

--- test/unit/index.test.js
+++ test/unit/index.test.js
@@ -5,7 +5,6 @@ var expect = require('expect.js');
 describe('models/index', function () {
   it('returns the task model', function () {
     var models = require('../../models');
-    console.log('hello');
     expect(models.Tasks).to.be.ok();
   });
 

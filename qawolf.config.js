7ccc9387a23991775464557397e2b485afdbd2fa
BryantLan12138 <48848298+BryantLan12138@users.noreply.github.com>
2020-04-13 20:46:46 +1000

    qawolf test

---
+++ qawolf.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  config: "node_modules/qawolf/js-jest.config.json",
+  rootDir: ".qawolf",
+  testTimeout: 60000,
+  useTypeScript: false,
+  QAW_HEADLESS: true,
+  watch: true
+}
